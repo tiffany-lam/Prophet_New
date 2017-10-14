51272d8f8bd8bc981642373d3b15a4fc55d57a46
nick.spencer2 <nick.spencer.cs@gmail.com>
2017-10-13 23:22:39 -0700

    Added Profile.java

---
+++ app/src/main/java/com/prophet/prophets/prophet/Profile.java
@@ -0,0 +1,44 @@
+package com.prophet.prophets.prophet;
+
+/**
+ * Created by nicks on 10/13/2017.
+ */
+
+public class Profile {
+    private String name;
+    private String familyMembers;
+    private String skills;
+    private String locations;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamilyMembers() {
+        return familyMembers;
+    }
+
+    public void setFamilyMembers(String familyMembers) {
+        this.familyMembers = familyMembers;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+
+    public String getLocations() {
+        return locations;
+    }
+
+    public void setLocations(String locations) {
+        this.locations = locations;
+    }
+}
