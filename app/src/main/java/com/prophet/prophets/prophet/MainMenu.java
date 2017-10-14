16e5988bb14c65245283f1d669580aff04f7900e
Al Zan <alfaza0965@outlook.com>
2017-10-13 23:03:15 -0700

    initial commit

---
+++ app/src/main/java/com/prophet/prophets/prophet/MainMenu.java
@@ -0,0 +1,20 @@
+package com.prophet.prophets.prophet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Paz_X on 10/13/2017.
+ */
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
